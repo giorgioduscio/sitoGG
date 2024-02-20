26e64604bac93cc724953ef9614e5663bfaaa0de
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-02-20 09:22:46 +0100

    Auto navigation

---
+++ menù.js
@@ -0,0 +1,15 @@
+document.body.querySelector("nav").innerHTML=`
+    <div>
+        <button id="logo">☰</button>
+        <button>Articoli</button>
+        <button>Articoli</button>
+        <button>Articoli</button>
+    </div>
+    <div id="search">
+        <input type="text" name="search" placeholder="🔍 Search"> 
+    </div>
+    <div>
+        <a href="/form.html" id="accedi">Accedi</a>
+        <a href="/form.html" id="iscrivi">Iscriviti</a>
+    </div>
+`;
