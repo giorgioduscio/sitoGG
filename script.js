9c4692348a9bfc4794e3594d5c4679a503418e0c
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-02-24 10:31:00 +0100

    armiAntiche.json

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("File Script");
+// TODO
+let requestURL=`/armiAntiche.json`; //indirizzo
+let request=new XMLHttpRequest(); //richiesta
+request.open("get",requestURL); //la richiesta apre il file dell'indirizzo
+request.responseType=`json`; //la interpreta come file JSON
+request.send(); //invia la richiesta al server
+
+request.onload=function () { //quando carica la richiesta..
+    var armi=request.response; //variabile=risposta alla richiesta
+    var array=armi.dueMani
+
+    for (let index = 0; index < array.length; index++) {
+        console.log(`Armi:`, armi.dueMani[index].nome);        
+    }
+
+}
