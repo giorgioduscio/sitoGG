82baf9fe78de6f53c69a61190a8c05821994b096
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-02-26 18:15:56 +0100

    device adapt

--- autoGeneration.js
+++ autoGeneration.js
@@ -1,18 +1,20 @@
 document.body.querySelector("nav").innerHTML=`
     <div>
-        <a href="/home.html">☰</a>
-        <a>Articoli</a>
-        <a>About</a>
-        <a>Prodotti</a>
+        <a id="dropButton">☰</a>
+        <a href="/home.html">
+            <img src="https://static.thenounproject.com/png/2505434-200.png">
+        </a>
+    </div>
+    <div id="search">
+        <input type="text" id="search" placeholder="Search"> 
     </div>
     <div>
-        <input type="text" name="search" id="search" placeholder="🔍 Search"> 
-    </div>
-    <div>
-        <a href="/form.html" id="accedi">Accedi</a>
-        <a href="/form.html" id="iscrivi">Iscriviti</a>
+        <a href="/form.html">
+            <img src="https://cdn-icons-png.freepik.com/512/6337/6337309.png" id="imageLogin">
+        </a>
     </div>
 `;
+
 
 document.body.querySelector("footer").innerHTML=`
     <column>
