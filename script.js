33e11012de839d7cfc72f4deb74b6e98dbf41e25
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-02-26 11:32:12 +0100

    formValidation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TODO
+// TODO dati json
 let requestURL=`/armiAntiche.json`; //indirizzo
 let request=new XMLHttpRequest(); //richiesta
 request.open("get",requestURL); //la richiesta apre il file dell'indirizzo
@@ -14,3 +14,36 @@ request.onload=function () { //quando carica la richiesta..
     }
 
 }
+
+//TODO form
+const form=document.querySelector(`#form`);
+const username=document.querySelector(`#username`);
+const email=document.querySelector(`#email`);
+const password=document.querySelector(`#password`);
+const errorMessage=document.querySelector(`#errorMessage`);
+
+form.addEventListener(`submit`,(e)=>{
+    let messages=[];
+
+    if (password.value===''||password.value===null) {
+        messages.push('- Password obbligatoria');
+        password.style.borderLeft='5px solid red';
+    }
+    if (password.value.length<=5||password.value.length>=21) {
+        messages.push('- La password deve avere tra 6 e 20 caratteri');
+        password.style.borderLeft='5px solid red';
+        password.style.color='red';
+    }
+
+    if (email.value===''||email.value===null) {
+        messages.push('- Email obbligatoria');
+        email.style.borderLeft='5px solid red';
+    }
+
+
+    if (messages.length>0) {
+        e.preventDefault();
+        errorMessage.style.display='block';
+        errorMessage.innerText=messages.join(`\n`)
+    }
+})
